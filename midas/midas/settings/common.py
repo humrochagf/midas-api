59c03ab980cda6b977388b1ca59a738db356ceb4
Humberto Rocha <humrochagf@gmail.com>
2017-12-29 18:26:33 -0200

    Add djangorest-jwt to use jwt token login

--- midas/midas/settings/common.py
+++ midas/midas/settings/common.py
@@ -46,6 +46,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'midas.wsgi.application'
 
 
+# REST Framework configs
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
--- midas/midas/urls.py
+++ midas/midas/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('login/', obtain_jwt_token),
     path('', include('finance.urls'))
 ]
